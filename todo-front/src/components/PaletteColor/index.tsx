26499440d562f5619fc07ae9b29ab4c074a76863
evandro <evandroivandro65@gmail.com>
2025-04-02 20:10:23 +0100

    add palette color in Palette Color component

--- todo-front/src/components/PaletteColor/index.tsx
+++ todo-front/src/components/PaletteColor/index.tsx
@@ -1,21 +1,14 @@
 "use client";
-import { useState } from "react";
-import { Toggle } from "@/components/ui/toggle";
+import ColorIcon from "@/assets/icons/colorIcon";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import ColorIcon from "@/assets/icons/colorIcon";
+import { Toggle } from "@/components/ui/toggle";
+import { useState } from "react";
 
-interface HandlePopoverChangeProps {
-  open: boolean;
-}
-
-interface PaletteColorProps {
-  onChangeColorCard: (color: string) => void;
-}
-
+// Array de opções de cor
 const colorOptions = [
   "#ffffff",
   "#BAE2FF",
@@ -31,22 +24,25 @@ const colorOptions = [
   "#A99A7C",
 ];
 
+interface PaletteColorProps {
+  onChangeColorCard: (color: string) => void;
+}
+
 function PaletteColor({ onChangeColorCard }: PaletteColorProps) {
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
 
-  const handlePopoverChange = (open: HandlePopoverChangeProps["open"]) => {
-    setIsPopoverOpen(open);
-  };
+  // Função para alternar o estado do popover
+  const togglePopover = (open: boolean) => setIsPopoverOpen(open);
 
   return (
-    <Popover onOpenChange={handlePopoverChange}>
+    <Popover onOpenChange={togglePopover}>
       <PopoverTrigger asChild>
         <Toggle
-          className={`cursor-pointer hover:bg-[#ffe3b388] rounded-full
+          className={`cursor-pointer hover:bg-[#ffe3b388] rounded-full 
             ${isPopoverOpen ? "bg-[#FFE3B3]" : "bg-transparent"} 
             transition-colors duration-300 ease-in-out`}
         >
-          <ColorIcon className="w-4 h-4 " color="#FFA000" />
+          <ColorIcon className="w-4 h-4" color="#FFA000" />
         </Toggle>
       </PopoverTrigger>
       <PopoverContent side="bottom" align="start" className="w-64 sm:w-lg">
@@ -54,10 +50,11 @@ function PaletteColor({ onChangeColorCard }: PaletteColorProps) {
           {colorOptions.map((color) => (
             <div
               key={color}
-              className="w-9 h-9 rounded-full cursor-pointer "
+              className="w-9 h-9 rounded-full cursor-pointer"
               style={{ backgroundColor: color }}
               onClick={() => onChangeColorCard(color)}
-            ></div>
+              aria-label={`Selecionar cor ${color}`} // Acessibilidade
+            />
           ))}
         </div>
       </PopoverContent>
